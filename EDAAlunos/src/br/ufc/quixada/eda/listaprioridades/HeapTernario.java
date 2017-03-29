bd2f67b58b23283a2852e551edc7f9af1a861f86
alex <alex@alex-270E5J-2570EJ>
2017-03-29 20:52:33 -0300

    HeapTernario

--- EDAAlunos/src/br/ufc/quixada/eda/listaprioridades/HeapTernario.java
+++ EDAAlunos/src/br/ufc/quixada/eda/listaprioridades/HeapTernario.java
@@ -1,5 +1,7 @@
 package br.ufc.quixada.eda.listaprioridades;
 
+import java.util.List;
+
 public class HeapTernario{
 	private int nMaximo = 0;
 	private int vetor[] = null;
@@ -9,33 +11,103 @@ public class HeapTernario{
 		nMaximo = Nmaximo;
 		vetor = new int[Nmaximo];
 	}
+	void swap(int a, int b){
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+	
+	public void subir(int i){
+		int j = (i+1)/3;
+		if(j>=1){
+			if(vetor[j] < vetor[i]){
+			swap(vetor[j], vetor[i]);
+			subir(j);
+		  }   
+		 }
+	}
 
-private void subir(int i){
-	int j = (i+1)/3;
-	if(j>=1){
-	 if(vetor[j]<vetor[i]){
-	  int aux=vetor[j];
-	  vetor[j]=vetor[i];
-	  vetor[i]=aux;
-	  subir(j);
-	  }   
-	 }
-}
-
-private void descer(int i){
-	int j=(3*i)-1;
-	if(j<=n){
-		if(j+2 <=n){
-			
-		}
-		else(vetor[j+1] > vetor[j] && ((j+1) == n)){
-			j++;
-			if(vetor[j]>vetor[i]){
-				 int aux=vetor[j];
-				 vetor[j]=vetor[i];
-				 vetor[i]=aux;
+	public void descer(int i){
+		int j=(3*i)-1; //filho da esquerda
+			if(j <= n ){
+				int temp = 0;
+				if(vetor[j+2] <= n){
+					if(vetor[j+1] < vetor[j+2])
+						temp = j+2;
+					else
+						temp = j+1;
+				}
+				if(vetor[temp] < vetor[j]){
+					if(vetor[i] < vetor[j]){
+						swap(vetor[j], vetor[i]);
+						descer(j);
+					}
+				}
+				else if(vetor[i] < vetor[temp]){
+					swap(vetor[i], vetor[temp]);
+					descer(temp);
+				}
+			}
+			else{
+				if(j+1 <= n && vetor[j] < vetor[j+1]){
+					j = j+1;
+					if(vetor[i] < vetor[j]){
+						swap(vetor[i], vetor[j]);
+						descer(j);
+					}
+				}
 			}
 		}
-	 }
+	
+	public void construir(List<Integer> entrada){
+		for(int i = 1; i <= entrada.size(); i++){
+			vetor[i] = entrada.get(i-1);
+			n++;
+		}
+		for(int j = (n/3)+1; j > 1; j--){
+			descer(j);
+		}
+	}
+	
+	public int getMaxPrioridade(){
+		return vetor[0];
+	}
+	
+	public int remove(){
+		int aux = 0;
+		if(n >= 1){
+			aux= vetor[0];
+			int temp = vetor[0];
+			vetor[0] = vetor[n-1];
+			vetor[n-1] = temp;
+			n--;
+			descer(0);
+		}
+		return aux;
+	}
+
+
+	public void inserir(int prioridade){
+		if(n+1 <= nMaximo){
+			vetor[n+1] = prioridade;
+			n++;
+			subir(n);
+		}
+	}
+	
+	public void alterarPrioridade(int prioridade, int novaPrioridade){
+		for(int i = 0; i < n; i++){
+			if(vetor[i] == prioridade){
+				if(vetor[i] < novaPrioridade){
+					vetor[i] = novaPrioridade;
+					subir(i);
+				}
+				else{
+					vetor[i] = novaPrioridade;
+					descer(i);
+				}
+				break;
+			}
+		}
 	}
 }
