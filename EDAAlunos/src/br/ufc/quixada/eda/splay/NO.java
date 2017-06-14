7eadbb99762846eeb75c1e1e9c5d69d08110329e
alex <alex@alex-270E5J-2570EJ>
2017-06-13 23:52:33 -0300

    Splay

--- EDAAlunos/src/br/ufc/quixada/eda/splay/NO.java
+++ EDAAlunos/src/br/ufc/quixada/eda/splay/NO.java
@@ -1,5 +1,34 @@
 package br.ufc.quixada.eda.splay;
 
-public class NO {
-
+public class NO<T> {
+	private int key;
+	private NO<T> left,right;
+	private T infor;
+	
+	public int getKey() {
+		return key;
+	}
+	public void setKey(int key) {
+		this.key = key;
+	}
+	public NO<T> getRight() {
+		return right;
+	}
+	public void setRight(NO<T> right) {
+		this.right = right;
+	}
+	public NO<T> getLeft() {
+		return left;
+	}
+	public void setLeft(NO<T> left) {
+		this.left = left;
+	}
+	public T getInfor() {
+		return infor;
+	}
+	public void setInfor(T infor) {
+		this.infor = infor;
+	}
+	
+	
 }
--- EDAAlunos/src/br/ufc/quixada/eda/splay/splay.java
+++ EDAAlunos/src/br/ufc/quixada/eda/splay/splay.java
@@ -1,5 +1,78 @@
 package br.ufc.quixada.eda.splay;
 
-public class splay {
+public class splay<T> {
+	private NO<T> raiz = null;
+
+	private NO<T> splay(NO<T> raiz, int key){
+		if(raiz == null)
+			return null;
+		if(raiz.getKey() == key) 
+			return raiz;
+		if(key < raiz.getKey()){
+			if(raiz.getLeft() != null){
+				raiz.setLeft(splay(raiz.getLeft(), key));
+				raiz = rotacaoDireita(raiz);
+			}
+		}else if (key > raiz.getKey()){
+			if(raiz.getRight() != null){
+				raiz.setRight(splay(raiz.getRight(),key));
+				raiz = rotacaoEsquerda(raiz);
+			}
+		}
+		return raiz;
+	}
+	
+	NO<T> rotacaoDireita(NO<T> no){
+		NO<T> tmp = no.getLeft();
+		no.setLeft(no.getLeft().getRight());
+		tmp.setRight(no);
+		return tmp;
+
+	}
+
+	NO<T> rotacaoEsquerda(NO<T> no){
+		NO<T> tmp = no.getRight();
+		no.setLeft(no.getRight().getLeft());
+		tmp.setLeft(no);
+		return tmp;
+
+	}
+
+	public NO<T> buscar(int key){
+		raiz = splay(raiz,key);
+		if(raiz != null && raiz.getKey() == key)
+			return raiz;
+		return null;
+	}
+
+	public void inserir(int key){
+		raiz = splay(raiz, key);
+		if(raiz == null || raiz.getKey() == key) 
+			return;
+		NO<T> tmp = new NO<T>();
+		tmp.setRight(raiz);
+		tmp.setLeft(raiz.getLeft());
+		raiz = tmp;
+	}
+
+	public void remover(int key){
+		raiz = splay(raiz, key);
+		if(raiz.getKey() != key || raiz == null )
+			return;
+		else{
+			NO<T> tmp = splay(raiz.getLeft(), key);
+			tmp.setRight(raiz.getRight());
+			raiz = tmp;
+		}
+	}
+
+
+	public NO<T> getRaiz() {
+		return raiz;
+	}
+
+	public void setRaiz(NO<T> raiz) {
+		this.raiz = raiz;
+	}
 
 }
